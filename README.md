33b6de6e8238aafe20a7a4d5f6a1a9f69ec1381a
Michael Bauer <michael@claimfound.com>
2020-10-04 09:59:18 -0400

    added env template, password reset email functionality

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # django-demo
 A small demo app made with Django
+
+To salt db:
+- python manage.py shell
+- exec(open('./utils/add_posts.py').read())
--- app/app/settings.py
+++ app/app/settings.py
@@ -125,3 +125,11 @@ LOGIN_URL = "login"
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = "587"
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+
